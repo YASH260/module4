e65aef6ce1288738b8dbdaaf33dff781a281cdb2
Yash Singh <syash5551@gmail.com>
2021-04-17 17:36:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
+(function(window) {
 
+  var byeSpeaker = {};
+  
+  var speakWord = "Good Bye";
+ 
+  byeSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+  }
+  window.byeSpeaker = byeSpeaker;
+  
+  })(window);
+  (function(window) {
+
+    var helloSpeaker = {};
+    var speakWord = "Hello";
+  
+    helloSpeaker.speak = function (name) {
+      console.log(speakWord + " " + name);
+    }
+  
+    window.helloSpeaker = helloSpeaker;
+  
+  })(window);
 (function () {
 
     var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
